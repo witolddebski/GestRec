b8dec8db54fc9b3b0c9e850c14746f27cfb481b1
witolddebski <witold.debski.stud@pw.edu.pl>
2023-07-01 14:28:38 +0200

    create class structure

--- main.py
+++ main.py
@@ -3,12 +3,37 @@ import torch.nn as nn
 import torchvision
 
 
+class Recognizer:
+    """
+    Perform overall recognition process
+    """
+
+    def __init__(self):
+        self.detector = Detector()
+        self.analyzer = Analyzer()
+
+
+class Detector:
+    """
+    Detect and classify gestures from images
+    """
+    def __init__(self):
+        self.model = torchvision.models.resnet34(progress=True)
+        self.model.fc = nn.Linear(self.model.fc.in_features, 20)
+        self.model.load_state_dict(torch.load('models/resnet34.zip', map_location=torch.device('cpu')))
+
+
+class Analyzer:
+    """
+    Analyze Detector results across frames
+    """
+    def __call__(self, gesture: int):
+        return gesture
+
+
 if __name__ == '__main__':
-    recognizer = torchvision.models.resnet34(progress=True)
-    num_features = recognizer.fc.in_features
-    recognizer.fc = nn.Linear(num_features, 20)
-    recognizer.load_state_dict(torch.load('models/resnet34.zip', map_location=torch.device('cpu')))
-    print(recognizer)
+    rec = Recognizer()
+    print(rec.detector.model)
 
 
 # TODO:
