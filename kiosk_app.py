2fb7d77b87e72b8de318c85513b1446880ce9114
witolddebski <witold.debski.stud@pw.edu.pl>
2023-07-09 22:07:27 +0200

    display frame rate counter

--- kiosk_app.py
+++ kiosk_app.py
@@ -1,7 +1,7 @@
 import gesture
 import cv2 as cv
-from PIL import Image
 import time
+from PIL import Image
 
 
 class Kiosk:
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     kiosk = Kiosk()
     cap = cv.VideoCapture(0, cv.CAP_DSHOW)
     print(cap.isOpened())
+    prev_time = time.time()
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -24,9 +25,13 @@ if __name__ == '__main__':
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         result = kiosk(Image.fromarray(image))
         image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-        cv.putText(image, result, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0))
-        cv.imshow('camera', image) # you can do cv.flip(image, 1)
+        cv.putText(image, result, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0))
+
+        curr_time = time.time()
+        fps = int(1 / (curr_time - prev_time))
+        cv.putText(image, str(fps), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255))
+        cv.imshow('camera', image)  # you can do cv.flip(image, 1)
+        prev_time = curr_time
         if cv.waitKey(5) & 0xFF == 27:
             break
     cap.release()
-
