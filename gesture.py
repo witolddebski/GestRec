43025a74eac470d338b9291198ae032ef0914c2a
witolddebski <witold.debski.stud@pw.edu.pl>
2023-07-10 21:24:05 +0200

    add jit script / trace option, add mobilenet

--- gesture.py
+++ gesture.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import os
+import time
 from PIL import Image
 
 
@@ -24,19 +25,36 @@ class Detector:
     Detect and classify gestures from images
     """
 
-    def __init__(self):
-        self.model = torchvision.models.resnet34(progress=True)
-        self.model.fc = nn.Linear(self.model.fc.in_features, 20)
-        self.model.load_state_dict(torch.load('models/resnet34.zip', map_location=torch.device('cpu')))
+    def __init__(self, model='mobilenet', jit_trace=True):
+        self.classes = ['0', '1', '10', '11', '12', '13', '14', '15', '16', '17',
+                        '18', '19', '2', '3', '4', '5', '6', '7', '8', '9']
+
+        if model == 'resnet34':
+            self.model = torchvision.models.resnet34()
+            self.model.fc = nn.Linear(self.model.fc.in_features, len(self.classes))
+            self.model.load_state_dict(torch.load('models/resnet34.zip', map_location=torch.device('cpu')))
+        if model == 'mobilenet':
+            self.model = torchvision.models.mobilenet_v3_large()
+            num_features = self.model.classifier._modules['3'].in_features
+            self.model.classifier._modules['3'] = nn.Linear(num_features, len(self.classes), bias=True)
+            self.model.load_state_dict(torch.load('models/mobilenet_v3_large.zip', map_location=torch.device('cpu')))
+
         self.transformer = transforms.Compose([
             transforms.Resize([512, 512]),
             # transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
-        self.classes = ['0', '1', '10', '11', '12', '13', '14', '15', '16', '17',
-                        '18', '19', '2', '3', '4', '5', '6', '7', '8', '9']
+
         self.model.eval()
+
+        if jit_trace:
+            sample_image = Image.open("test_images/16.jpg")
+            sample_image = self.transformer(sample_image)
+            sample_image = sample_image.unsqueeze(0)
+            self.model = torch.jit.optimize_for_inference(torch.jit.trace(self.model, sample_image))
+        else:
+            self.model = torch.jit.optimize_for_inference(torch.jit.script(self.model))
 
     def __call__(self, img_raw):
         # optimize by calling ahead of time?
@@ -54,17 +72,35 @@ class Analyzer:
     Analyze Detector results across frames
     """
 
-    def __call__(self, gesture):
-        return gesture
+    def __init__(self, model='mobilenet', jit_trace=True):
+        self.current_gest = '19'
+        self.counter = 0
+        self.predicted_gest = '19'
+
+    def __call__(self, detected_gest):
+        if detected_gest != self.current_gest and detected_gest == self.predicted_gest:
+            if self.counter < 3:
+                self.counter += 1
+            else:
+                self.counter = 0
+                self.current_gest = detected_gest
+        else:
+            self.predicted_gest = detected_gest
+
+        return self.current_gest
 
 
 if __name__ == '__main__':
     rec = Recognizer()
 
+    start_time = time.time()
     for x in os.listdir('test_images/series_1'):
         image = Image.open("test_images/series_1/" + x)
         print(rec(image), ":", x[x.find('_')+1:x.rfind('.')])
+    end_time = time.time()
+    frames = len(os.listdir('test_images/series_1'))
+    print("Frame rate: ", frames / (end_time - start_time))
 
 # TODO:
-# run model on an image
+# try better preprocessing, as in raspberry Pi example
 # try training resnet with pretrained=True
