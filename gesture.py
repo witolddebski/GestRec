6623e1df6389b4c45f14c74a9cfd851867e12425
witolddebski <witold.debski.stud@pw.edu.pl>
2023-07-24 17:35:39 +0200

    improve code quality

--- gesture.py
+++ gesture.py
@@ -12,8 +12,8 @@ class Recognizer:
     Perform overall recognition process
     """
 
-    def __init__(self, model='mobilenet'):
-        self.detector = Detector(model)
+    def __init__(self, model_name: str = 'mobilenet224'):
+        self.detector = Detector(model_name)
         self.analyzer = Analyzer()
 
     def __call__(self, img):
@@ -22,14 +22,14 @@ class Recognizer:
 
 class Detector:
     """
-    Detect and classify gestures from images
+    Detect and classify gestures from images.
     """
 
-    def __init__(self, model='mobilenet', jit_trace=True):
-        self.classes = ['0', '1', '10', '11', '12', '13', '14', '15', '16', '17',
-                        '18', '19', '2', '3', '4', '5', '6', '7', '8', '9']
+    def __init__(self, model_name: str, jit_trace: bool = True):
+        self.classes = ['00', '01', '10', '11', '12', '13', '14', '15', '16', '17',
+                        '18', '19', '02', '03', '04', '05', '06', '07', '08', '09']
 
-        if model == 'resnet34':
+        if model_name == 'resnet34':
             self.model = torchvision.models.resnet34()
             self.model.fc = nn.Linear(self.model.fc.in_features, len(self.classes))
             self.model.load_state_dict(torch.load('models/resnet34.zip', map_location=torch.device('cpu')))
@@ -39,7 +39,7 @@ class Detector:
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
 
-        elif model == 'mobilenet':
+        elif model_name == 'mobilenet':
             self.model = torchvision.models.mobilenet_v3_large()
             num_features = self.model.classifier._modules['3'].in_features
             self.model.classifier._modules['3'] = nn.Linear(num_features, len(self.classes), bias=True)
@@ -50,7 +50,7 @@ class Detector:
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
 
-        elif model == 'mobilenet224':
+        elif model_name == 'mobilenet224':
             self.model = torchvision.models.mobilenet_v3_large()
             num_features = self.model.classifier._modules['3'].in_features
             self.model.classifier._modules['3'] = nn.Linear(num_features, len(self.classes), bias=True)
@@ -61,8 +61,8 @@ class Detector:
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
-
-
+        else:
+            raise ValueError('model_name must be one of available models')
 
         self.model.eval()
 
@@ -74,8 +74,12 @@ class Detector:
         else:
             self.model = torch.jit.optimize_for_inference(torch.jit.script(self.model))
 
-    def __call__(self, img_raw):
-        # optimize by calling ahead of time?
+    def __call__(self, img_raw) -> str:
+        """
+        Perform inference of the gesture in an image.
+        :param img_raw: PIL image to be analyzed. The image must be flipped horizontally, as when using a frontal camera
+        :return: string with class number of detected gesture
+        """
         img = self.transformer(img_raw)
         img = img.unsqueeze(0)
 
@@ -90,14 +94,15 @@ class Analyzer:
     Analyze Detector results across frames
     """
 
-    def __init__(self, model='mobilenet', jit_trace=True):
+    def __init__(self, model='mobilenet', jit_trace=True, threshold=3):
         self.current_gest = '19'
         self.counter = 0
         self.predicted_gest = '19'
+        self.threshold = threshold
 
-    def __call__(self, detected_gest):
+    def __call__(self, detected_gest: str) -> str:
         if detected_gest != self.current_gest and detected_gest == self.predicted_gest:
-            if self.counter < 3:
+            if self.counter < self.threshold:
                 self.counter += 1
             else:
                 self.counter = 0
@@ -109,16 +114,20 @@ class Analyzer:
 
 
 if __name__ == '__main__':
-    rec = Recognizer(model='mobilenet224')
+    rec = Recognizer(model_name='mobilenet224')
 
     start_time = time.time()
     for x in os.listdir('test_images/series_1'):
         image = Image.open("test_images/series_1/" + x)
-        print(rec(image), ":", x[x.find('_')+1:x.rfind('.')])
+        print(rec(image), ":", x[x.find('_') + 1:x.rfind('.')])
     end_time = time.time()
     frames = len(os.listdir('test_images/series_1'))
     print("Frame rate: ", frames / (end_time - start_time))
 
 # TODO:
 # try better preprocessing, as in raspberry Pi example
-# try training resnet with pretrained=True
+# testing
+# add possibility of modifying detector & analyzer
+# add type hints
+# create subclass handling camera for Kiosk app
+# create Kiosk app logic
--- kiosk_app.py
+++ kiosk_app.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 class Kiosk:
     def __init__(self):
-        self.rec = gesture.Recognizer(model='mobilenet224')
+        self.rec = gesture.Recognizer(model_name='mobilenet224')
 
     def __call__(self, img):
         return self.rec(img)
@@ -27,12 +27,12 @@ if __name__ == '__main__':
         result = kiosk(Image.fromarray(image))
         image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
         image.flags.writeable = True
-        cv.putText(image, result, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0))
+        cv.putText(image, result, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), thickness=2)
 
         curr_time = time.time()
         fps = int(1 / (curr_time - prev_time))
-        cv.putText(image, str(fps), (7, 70), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255))
-        cv.imshow('camera', image)  # you can do cv.flip(image, 1)
+        cv.putText(image, str(fps), (50, image.shape[0] - 50), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), thickness=2)
+        cv.imshow('camera', image)
         prev_time = curr_time
         if cv.waitKey(5) & 0xFF == 27:
             break
