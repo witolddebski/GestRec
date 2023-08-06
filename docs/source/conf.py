bb400437c2b6f4ed831395a8ae7c4ecd035c87b1
witolddebski <witold.debski.stud@pw.edu.pl>
2023-08-06 21:55:47 +0200

    create class reference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import pathlib
+import sys
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,12 +15,15 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
