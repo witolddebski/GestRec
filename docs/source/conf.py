d7ee4ec34b11db29ca217b922c7f2a1f3149d138
witolddebski <witold.debski.stud@pw.edu.pl>
2023-08-12 19:21:34 +0200

    add master_doc variable

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,10 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 import pathlib
 import sys
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'GestRec'
 copyright = '2023, Witold Debski'
 author = 'Witold Debski'
 release = '0.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 extensions = [
@@ -24,7 +15,7 @@ extensions = [
 ]
 
 autodoc_class_signature = "separated"
-
+master_doc = 'index'
 autodoc_default_options = {
     'member-order': 'bysource',
     'special-members': '__init__, __call__',
@@ -32,9 +23,6 @@ autodoc_default_options = {
 }
 templates_path = ['_templates']
 exclude_patterns = []
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_theme_path = []
